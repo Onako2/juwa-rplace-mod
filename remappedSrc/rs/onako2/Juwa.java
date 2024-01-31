d7e0c174616447d8e556f9f41d9acd67bf4f6474
Onako2 <FT4e75c7ggkgln603mrM>
2024-01-31 19:37:36 +0100

    Updated that it now doesn't freeze while updating

---
+++ remappedSrc/rs/onako2/Juwa.java
@@ -0,0 +1,35 @@
+package rs.onako2;
+
+import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+
+public class Juwa implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("juwa-place");
+
+    @Override
+    public void onInitialize() {
+        CompletableFuture.runAsync(() -> {
+            LOGGER.info("Updating schematics");
+            try {
+                Schematic.downloadSchematics();
+                Schematic.unzipSchematics();
+                LOGGER.info("Updated the schematics!");
+            } catch (Exception e) {
+                LOGGER.error("Failed updating schematics");
+                throw new CompletionException(e);
+            }
+        }).exceptionally(ex -> {
+            LOGGER.error("Failed updating schematics!");
+            return null;
+        });
+        
+
+    }
+}
--- src/client/java/rs/onako2/InitCommands.java
+++ src/client/java/rs/onako2/InitCommands.java
@@ -1,9 +1,14 @@
 package rs.onako2;
 
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.text.Text;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 public class InitCommands implements ClientModInitializer {
     @Override
@@ -11,13 +16,22 @@ public class InitCommands implements ClientModInitializer {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             dispatcher.register(
                     ClientCommandManager.literal("refresh-schematics").executes(context -> {
+
                         context.getSource().sendFeedback(Text.literal("Updating schematics.."));
 
-                        
-                        Schematic.downloadSchematics();
-                        Schematic.unzipSchematics();
+                        CompletableFuture.runAsync(() -> {
+                            try {
+                                Schematic.downloadSchematics();
+                                Schematic.unzipSchematics();
+                                context.getSource().sendFeedback(Text.literal("Schematics updated!"));
+                            } catch (Exception e) {
+                                throw new CompletionException(e);
+                            }
+                        }).exceptionally(ex -> {
+                            context.getSource().sendFeedback(Text.literal("Failed to update schematics: " + ex.getMessage()));
+                            return null;
+                        });
 
-                        context.getSource().sendFeedback(Text.literal("Schematics updated!"));
                         return 0;
                     }));
         });
